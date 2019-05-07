538e24e619b1024999563c47fe5714bd5142c58f
Pascal Kleindienst <pascal.kleindienst@solution360.de>
2019-05-07 11:33:27 +0200

    refactoring + added hooks for cancel and update order

--- Subscriber/Order.php
+++ Subscriber/Order.php
@@ -11,38 +11,107 @@ use BilliePayment\Models\Api;
 class Order implements SubscriberInterface
 {
     /**
+     * Canceled Order Code
+     * @var integer
+     */
+    const ORDER_CANCELED = 4;
+
+
+    /**
+     * Shipped Order Code
+     * @var integer
+     */
+    const ORDER_SHIPPED = 7;
+
+    /**
      * {@inheritdoc}
      */
     public static function getSubscribedEvents()
     {
         return [
-            'Shopware_CronJob_CheckOrderStatus' => 'onCheckOrderStatus',
+            'Shopware_CronJob_CheckOrderStatus'                          => 'onCheckOrderStatus',
+            'Enlight_Controller_Action_PostDispatchSecure_Backend_Order' => 'onSaveOrder',
+            'Enlight_Controller_Action_PreDispatch_Backend_Order'        => 'onBeforeSaveOrder'
         ];
     }
 
     /**
-     * Check order state and notify billie if order is shipped.
+     * Send updates to billie if order is changed.
      *
-     * @param \Shopware_Components_Cron_CronJob $job
+     * @param \Enlight_Event_EventArgs $args
      * @return void
      */
-    public function onCheckOrderStatus(\Shopware_Components_Cron_CronJob $job)
+    public function onBeforeSaveOrder(\Enlight_Event_EventArgs $args)
     {
-        // load all billie orders that are in the 'created' state
-        $models = Shopware()->Container()->get('models');
-        $api    = $models->getRepository(Api::class);
-        $rows   = $api->findBy(['state' => 'created']);
+        /** @var Shopware_Controllers_Backend_Order $controller */
+        $controller = $args->getSubject();
+        $request    = $controller->Request();
 
-        // Check each order if it is shipped, and if so, tell billie about it and update state
-        foreach ($rows as $row) {
-            $order = $row->getOrder();
-
-            if ('completely_delivered' === $order->getOrderStatus()->getName()) {
-                // TODO: run POST /v1/order/{order_id}/ship
-                // TODO: Flag billie state as 'shipped' (or declined based on api response)
-                $row->setState('shipped');
+        if ($request->getActionName() == 'save') {
+            $params = $request->getParams();
+            $order  = Shopware()->Models()->find('Shopware\Models\Order\Order', $params['id']);
+            
+            // Update Amount if changed
+            if ($order->getInvoiceAmount() != $params['invoiceAmount']) {
+                // TODO: run PATCH /v1/order/{order_id}
+                // TODO: print possible error message
+                // exit('{"success": false, "message": "Dies ist eine Fehlernachricht"}');
             }
         }
-        $models->flush();
+    }
+
+    /**
+     * Sent updates to billie if order is changed.
+     *
+     * @param \Enlight_Event_EventArgs $args
+     * @return void
+     */
+    public function onSaveOrder(\Enlight_Event_EventArgs $args)
+    {
+        /** @var Shopware_Controllers_Backend_Order $controller */
+        $controller = $args->getSubject();
+        $request    = $controller->Request();
+
+        if ($request->getActionName() !== 'save') {
+            $params = $request->getParams();
+
+            switch ($params['status']) {
+                // Order is canceled.
+                case self::ORDER_CANCELED:
+                    // TODO: run POST /v1/order/{order_1}/cancel
+                    // TODO: print possible error message
+                    $this->updateState($params['id'], 'canceled');
+                    exit('{"success": false, "message": "Dies ist eine Fehlernachricht"}');
+                    break;
+
+                // Order is shipped
+                case self::ORDER_SHIPPED:
+                    // TODO: run POST /v1/order/{order_id}/ship
+                    // TODO: Flag billie state as 'shipped' (or declined based on api response)
+                    $this->updateState($params['id'], 'shipped');
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /**
+     * Update local api order state.
+     *
+     * @param integer $order
+     * @param string $state
+     * @return void
+     */
+    protected function updateState($order, $state)
+    {
+        $models = Shopware()->Container()->get('models');
+        $api    = $models->getRepository(Api::class);
+        $entry  = $api->find($order);
+        
+        if ($entry) {
+            $entry->setState($state);
+            $models->flush();
+        }
     }
 }
